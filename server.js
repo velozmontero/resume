c099ec153450f11ec4e491fee92c4501cd71109c
Frank Veloz <velozmontero@gmail.com>
2018-08-22 18:55:21 -0400

    added images

--- server.js
+++ server.js
@@ -18,6 +18,29 @@ var transporter = nodemailer.createTransport({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+//Purpose of this is to enable cross domain requests
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', ['http://fveloz.techlaunch.io:8000', 'http://142.93.207.204:8000']);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 // Expose static content like css, js, images
 app.use('/', express.static(path.join(__dirname, 'public')));
 
